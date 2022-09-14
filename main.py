d7f52ae85e63f42679c8183f24a27a38cb9416df
Tran Xuan Loc <tranxuanloc19920@gmail.com>
2022-09-14 23:03:05 +0700

    take arguments for this script

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-# requirements: selenium
 import sys
+import os
 import logging
 import csv
+from argparse import ArgumentParser
 
 from selenium.webdriver import Firefox
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
@@ -13,18 +14,23 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 ROOT_URL = "https://congbobanan.toaan.gov.vn/0tat1cvn/ban-an-quyet-dinh/"
-COURT = "TAND tỉnh An Giang"
 
-# KEYWORD1 = "trọng tài"
-# KEYWORD2 = "PQTT"
-# KEYWORD3 = "nước ngoài"
-KEYWORDS = ["trọng tài", "PQTT", "nước ngoài"]
 
-# LEVEL = "TW"
-LEVEL = "CW"
-# LEVEL = "T"
+def load_keywords(keyword_file):
+    with open(keyword_file, "r") as f:
+        lines = f.readlines()
+        return [line.strip() for line in lines]
 
-START_AT_PAGE = 1
+
+def load_court_name(court_file):
+    with open(court_file, "r") as f:
+        lines = f.readlines()
+        if len(lines) == 0:
+            raise RuntimeError("No court name found")
+        elif len(lines) > 1:
+            raise RuntimeError("More than 1 court specified")
+
+        return lines[0].strip()
 
 
 def extract_info(element):
@@ -34,6 +40,33 @@ def extract_info(element):
 
 
 def main(args):
+    if not args.level and args.court:
+        raise Exception("Not specify court level but specify court name")
+
+    if os.path.exists(args.keyword_file):
+        keywords = load_keywords(args.keyword_file)
+    else:
+        raise Exception("Keyword file not found")
+
+    if args.court:
+        if os.path.exists(args.court):
+            court_name = load_court_name(args.court)
+        else:
+            raise Exception("Court file not found")
+
+    output_file = ""
+    if args.level:
+        output_file += args.level
+    if args.judgement:
+        output_file += args.judgement
+    if args.court:
+        output_file += args.court.split(".")[0]
+    if args.type:
+        output_file += str(args.type)
+
+    output_file += args.keyword_file.split(".")[0]
+    output_file += ".csv"
+
     opts = FirefoxOptions()
     opts.add_argument("--headless")
 
@@ -48,35 +81,61 @@ def main(args):
     )
     confirm_button.click()
 
-    cap_toa_an_options = browser.find_element(
-        By.ID, "ctl00_Content_home_Public_ctl00_Drop_Levels_top"
-    )
-    cap_toa_an_options.click()
-    cap_toa_an_dropdown = Select(cap_toa_an_options)
-    cap_toa_an_dropdown.select_by_value(LEVEL)
+    if args.level:
+        cap_toa_an_options = browser.find_element(
+            By.ID, "ctl00_Content_home_Public_ctl00_Drop_Levels_top"
+        )
+        cap_toa_an_options.click()
+        cap_toa_an_dropdown = Select(cap_toa_an_options)
+        cap_toa_an_dropdown.select_by_value(args.level)
 
-    # banan_quyetdinh_options = browser.find_element(
-    #     By.ID, "ctl00_Content_home_Public_ctl00_Drop_STATUS_JUDGMENT_SEARCH_top"
-    # )
-    # banan_quyetdinh_options.click()
-    # banan_quyetdinh_dropdown = Select(banan_quyetdinh_options)
-    # banan_quyetdinh_dropdown.select_by_value("0")  # Ban an
-    # banan_quyetdinh_dropdown.select_by_value("1")  # Quyet dinh
+    if args.judgement:
+        for attempt in range(3):
+            try:
+                banan_quyetdinh_options = browser.find_element(
+                    By.ID,
+                    "ctl00_Content_home_Public_ctl00_Drop_STATUS_JUDGMENT_SEARCH_top",
+                )
+                banan_quyetdinh_options.click()
+                banan_quyetdinh_dropdown = Select(banan_quyetdinh_options)
+                banan_quyetdinh_dropdown.select_by_value(args.judgement)
+            except StaleElementReferenceException:
+                continue
+            else:
+                break
+        else:  # no break
+            browser.close()
+            raise RuntimeError("Cannot select judgement/decision")
 
-    """
-    toaan_options = browser.find_element(
-        By.ID, "ctl00_Content_home_Public_ctl00_Ra_Drop_Courts_top_chosen"
-    )
-    toaan_options.click()
+    if args.court:
+        toaan_options = browser.find_element(
+            By.ID, "ctl00_Content_home_Public_ctl00_Ra_Drop_Courts_top_chosen"
+        )
+        toaan_options.click()
+        toaan_input = browser.find_element(
+            By.XPATH,
+            "//*[@class='chosen-search-input'][@type='text'][@autocomplete='off']",
+        )
+        toaan_input.send_keys(court_name)
+        toaan_input.send_keys(Keys.ENTER)
 
-    toaan_input = browser.find_element(
-        By.XPATH,
-        "//*[@class='chosen-search-input'][@type='text'][@autocomplete='off']",
-    )
-
-    toaan_input.send_keys(COURT)
-    toaan_input.send_keys(Keys.ENTER)
-    """
+    if args.type:
+        for attempt in range(3):
+            try:
+                loaivuviec_options = browser.find_element(
+                    By.ID,
+                    "ctl00_Content_home_Public_ctl00_Drop_CASES_STYLES_SEARCH_top",
+                )
+                loaivuviec_options.click()
+                loaivuviec_dropdown = Select(loaivuviec_options)
+                loaivuviec_dropdown.select_by_value(str(args.type))
+            except StaleElementReferenceException:
+                continue
+            else:
+                break
+        else:  # no break
+            browser.close()
+            raise RuntimeError("Cannot select")
 
     search_button = browser.find_element(
         By.ID, "ctl00_Content_home_Public_ctl00_cmd_search_banner"
@@ -86,70 +145,123 @@ def main(args):
     pagenum_button = browser.find_element(
         By.ID, "ctl00_Content_home_Public_ctl00_DropPages"
     )
-    # pagenum_button.click()
     pagenum_dropdown = Select(pagenum_button)
     num_pages = len(pagenum_dropdown.options)
     logging.info(f"Num pages: {num_pages}")
 
-    pagenum_dropdown.select_by_value(str(START_AT_PAGE))
+    if args.start_from > num_pages:
+        browser.close()
+        raise RuntimeError("Invalid start page")
 
-    logging.info(f"Start at page {START_AT_PAGE}")
-    for i in range(START_AT_PAGE, num_pages + 2):
-        current_page = str(i)
-        logging.info(f"Page: {current_page}")
-        result_list = browser.find_element(By.ID, "List_group_pub")
-        results = result_list.find_elements(By.CLASS_NAME, "list-group-item")
+    pagenum_dropdown.select_by_value(str(args.start_from))
 
-        found = []
-        for res in results:
-            # extract info here
+    logging.info(f"Start at page {args.start_from}")
+    try:
+        for i in range(args.start_from, num_pages + 1):
+            current_page = str(i)
+            logging.info(f"Page: {current_page}")
+            result_list = browser.find_element(By.ID, "List_group_pub")
+            results = result_list.find_elements(By.CLASS_NAME, "list-group-item")
+
+            found = []
+            for res in results:
+                # extract info here
+                try:
+                    if not any(kw in res.text for kw in keywords):
+                        continue
+
+                    matches = ",".join(kw for kw in keywords if kw in res.text)
+                    title, link = extract_info(res)
+                    print(",".join([matches, title, link]))
+                    found.append([matches, title, link])
+
+                except StaleElementReferenceException:
+                    logging.debug("stale!!!")
+
+            with open(output_file, "a") as f:
+                writer = csv.writer(f)
+                for row in found:
+                    writer.writerow(row)
+
             try:
-                if not any(kw in res.text for kw in KEYWORDS):
-                    continue
-
-                matches = ",".join(kw for kw in KEYWORDS if kw in res.text)
-                title, link = extract_info(res)
-                print(",".join([matches, title, link]))
-                found.append([matches, title, link])
-
-            except StaleElementReferenceException:
-                logging.debug("stale!!!")
-
-        with open(f"{LEVEL}.csv", "a") as f:
-            writer = csv.writer(f)
-            for row in found:
-                writer.writerow(row)
-
-        try:
-            wait = WebDriverWait(browser, 5)
-            _ = wait.until(
-                EC.text_to_be_present_in_element_value(
-                    (By.ID, "ctl00_Content_home_Public_ctl00_DropPages"),
-                    current_page,
+                wait = WebDriverWait(browser, 5)
+                _ = wait.until(
+                    EC.text_to_be_present_in_element_value(
+                        (By.ID, "ctl00_Content_home_Public_ctl00_DropPages"),
+                        current_page,
+                    )
                 )
-            )
-        except TimeoutException:
-            pass
+            except TimeoutException:
+                pass
 
-        wait2 = WebDriverWait(browser, 5)
-        for _ in range(3):
-            done = wait2.until(
-                EC.element_to_be_clickable(
-                    (By.ID, "ctl00_Content_home_Public_ctl00_cmdnext")
+            wait2 = WebDriverWait(browser, 5)
+            for _ in range(3):
+                done = wait2.until(
+                    EC.element_to_be_clickable(
+                        (By.ID, "ctl00_Content_home_Public_ctl00_cmdnext")
+                    )
                 )
+                if done:
+                    break
+
+            nextpage_button = browser.find_element(
+                By.ID, "ctl00_Content_home_Public_ctl00_cmdnext"
             )
-            if done:
-                break
-
-        nextpage_button = browser.find_element(
-            By.ID, "ctl00_Content_home_Public_ctl00_cmdnext"
-        )
-        nextpage_button.click()
-        browser.implicitly_wait(5)
-
-    browser.close()
+            nextpage_button.click()
+            browser.implicitly_wait(5)
+    except KeyboardInterrupt:
+        logging.info("Interrupted!")
+    finally:
+        browser.close()
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
-    main(sys.argv)
+    parser = ArgumentParser()
+    parser.add_argument(
+        "-l",
+        "--level",
+        type=str,
+        default="",
+        choices=["TW", "CW", "T", "H"],
+        required=False,
+        help="Court level",
+    )
+    parser.add_argument(
+        "-j",
+        "--judgement",
+        type=str,
+        default="",
+        choices=["0", "1"],
+        required=False,
+        help="Judgement or Decision",
+    )
+    parser.add_argument(
+        "-c",
+        "--court",
+        default=None,
+        required=False,
+        help="Path to file specify court name",
+    )
+    parser.add_argument(
+        "-t",
+        "--type",
+        default=None,
+        type=int,
+        choices=[50, 0, 1, 2, 3, 4, 5, 11],
+        required=False,
+        help="Case type",
+    )
+    parser.add_argument(
+        "--keyword_file",
+        required=True,
+    )
+    parser.add_argument(
+        "--start_from",
+        default=1,
+        type=int,
+        required=False,
+        help="Start from page",
+    )
+    args = parser.parse_args()
+    main(args)
